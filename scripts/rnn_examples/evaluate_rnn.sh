a64e93dc1136e59b0ad7b58e935562549d1347b4
Zimeng <zimeng@Zimengs-MacBook-Air.local>
2026-07-14 14:54:23 -0400

    update scripts

--- scripts/rnn_examples/evaluate_rnn.sh
+++ scripts/rnn_examples/evaluate_rnn.sh
@@ -2,18 +2,20 @@
 
 cd build
 
-INPUT_PARAMETERS="AltAGL AltB AltGPS AltMSL BaroA E1_CHT1 E1_CHT2 E1_CHT3 E1_CHT4 E1_EGT1 E1_EGT2 E1_EGT3 E1_EGT4 E1_FFlow E1_OilP E1_OilT E1_RPM FQtyL FQtyR GndSpd IAS LatAc NormAc OAT Pitch Roll TAS VSpd VSpdG WndDr WndSpd"
-OUTPUT_PARAMETERS="Pitch"
+# INPUT_PARAMETERS="AltAGL AltB AltGPS AltMSL BaroA E1_CHT1 E1_CHT2 E1_CHT3 E1_CHT4 E1_EGT1 E1_EGT2 E1_EGT3 E1_EGT4 E1_FFlow E1_OilP E1_OilT E1_RPM FQtyL FQtyR GndSpd IAS LatAc NormAc OAT Pitch Roll TAS VSpd VSpdG WndDr WndSpd"
+# OUTPUT_PARAMETERS="Pitch"
+INPUT_PARAMETERS="Conditioner_Inlet_Temp Conditioner_Outlet_Temp Coal_Feeder_Rate Primary_Air_Flow Primary_Air_Split System_Secondary_Air_Flow_Total Secondary_Air_Flow Secondary_Air_Split Tertiary_Air_Split Total_Comb_Air_Flow Supp_Fuel_Flow Main_Flm_Int" 
+OUTPUT_PARAMETERS="Main_Flm_Int" 
 
-exp_name="../test_output/c172_mpi"
+exp_name="../test_output/coal_mpi"
 for i in 0 1 2 3 4 5 6 7 8 9 
 do
 
-    genome_name="/Users/zimenglyu/Downloads/demo_new/global_best_genome_${i}.bin"
+    genome_name="../scripts/rnn_examples/demo_new/global_best_genome_${i}.bin"
 
-    out_dir="../test_output/c172_mpi/evaluation_results_new/$i"
+    out_dir="../test_output/coal_mpi/evaluation_results_new/$i"
     mkdir -p $out_dir
-    echo "Evaluating RNN on c172 dataset, results will be saved to: "$out_dir
+    echo "Evaluating RNN on coal dataset, results will be saved to: "$out_dir
 
     ./rnn_examples/evaluate_rnn \
     --testing_filenames ../datasets/2018_coal/burner_11.csv \
