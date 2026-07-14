5c797446b001d0e48247839b804be66aca15803b
Zimeng <zimeng@Zimengs-MacBook-Air.local>
2026-07-14 12:04:47 -0400

    Remove unused cnn, dataset, and tooling directories
    Drops cnn/cnn_examples/cnn_tests, datasets, graphviz_models, image_tools,
    images, multithreaded, opencl, results, scripts, test_output,
    visualization, and word_series; project still compiles without them.

--- visualization/generate_2020_hpml_plots.sh
+++
@@ -1,9 +0,0 @@
-python plot_examm_fitness.py 0.0 0.25 time "1 Island" island_1_time.png ~/Dropbox/_extended_diff_islands_populations/coal/island_1_pop_*
-python plot_examm_fitness.py 0.0 0.25 time "5 Islands" island_5_time.png ~/Dropbox/_extended_diff_islands_populations/coal/island_5_pop_*
-python plot_examm_fitness.py 0.0 0.25 time "10 Islands" island_10_time.png ~/Dropbox/_extended_diff_islands_populations/coal/island_10_pop_*
-python plot_examm_fitness.py 0.0 0.25 time "20 Islands" island_20_time.png ~/Dropbox/_extended_diff_islands_populations/coal/island_20_pop_*
-
-python plot_examm_fitness.py 0.0 0.25 inserted "1 Island" island_1_inserted.png ~/Dropbox/_extended_diff_islands_populations/coal/island_1_pop_*
-python plot_examm_fitness.py 0.0 0.25 inserted "5 Islands" island_5_inserted.png ~/Dropbox/_extended_diff_islands_populations/coal/island_5_pop_*
-python plot_examm_fitness.py 0.0 0.25 inserted "10 Islands" island_10_inserted.png ~/Dropbox/_extended_diff_islands_populations/coal/island_10_pop_*
-python plot_examm_fitness.py 0.0 0.25 inserted "20 Islands" island_20_inserted.png ~/Dropbox/_extended_diff_islands_populations/coal/island_20_pop_*
